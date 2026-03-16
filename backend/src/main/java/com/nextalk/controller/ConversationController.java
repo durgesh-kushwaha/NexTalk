384c71b4490ef649145570eec47efc8e3b360e85
durgesh-kushwaha <durgeshcgc@gmail.com>
2026-03-16 13:36:37 +0530

    refactor: Organize import statements in ConversationController for better readability

--- backend/src/main/java/com/nextalk/controller/ConversationController.java
+++ backend/src/main/java/com/nextalk/controller/ConversationController.java
@@ -1,16 +1,22 @@
 package com.nextalk.controller;
 
-import com.nextalk.dto.ConversationDTO;
-import com.nextalk.service.ConversationService;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Map;
+import com.nextalk.dto.ConversationDTO;
+import com.nextalk.service.ConversationService;
 
 @RestController
 @RequestMapping("/api/conversations")
